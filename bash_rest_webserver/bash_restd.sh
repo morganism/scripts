3c0e01c799037a0ae56f9ae0a7a55075250385b3
Morgan Sziraki <morgan.sziraki@gmail.com>
2015-06-07 16:44:59 +0100

    help

--- bash_rest_webserver/bash_restd.sh
+++ bash_rest_webserver/bash_restd.sh
@@ -9,24 +9,27 @@ while true
 do
   cat HTTP_PIPE | nc -l $PORT > >( 
     export HTTP_REQUEST=
-    while read line
+    while read input
     do
-      line=$(echo "$line" | tr -d '[\r\n]')
+      input=$(echo "$input" | tr -d '[\r\n]')
 
-      if echo "$line" | grep -qE '^GET /' 
+      if echo "$input" | grep -qE '^GET /' 
       then
-        HTTP_REQUEST=$(echo "$line" | cut -d ' ' -f2) # get the request part
-      elif [ "x$line" = x ] # empty .. EOR
+        HTTP_REQUEST=$(echo "$input" | awk '{print $2}') # get the request part
+      elif [ "x$input" = x ] # empty .. EOR
       then
         HTTP_STATUS_200="HTTP/1.1 200 OK"
-        HTTP_REQ_LOC="REST Loc:"
+        HTTP_REQ_LOC="Target:"
         HTTP_STATUS_404="HTTP/1.1 404 Not Found"
 
-        # ANY SCRIPT HERE
+        # SCRIPT HERE
 
         if echo $HTTP_REQUEST | grep -qE '^/echo/'
         then
             printf "%s\n%s %s\n\n%s\n" "$HTTP_STATUS_200" "$HTTP_REQ_LOC" $HTTP_REQUEST ${HTTP_REQUEST#"/echo/"} > HTTP_PIPE
+        elif echo $HTTP_REQUEST | grep -qE '^/help'
+        then
+            echo "Supported targets: /help /echo/something /src" > HTTP_PIPE
         elif echo $HTTP_REQUEST | grep -qE '^/src'
         then
             cat $0 > HTTP_PIPE
